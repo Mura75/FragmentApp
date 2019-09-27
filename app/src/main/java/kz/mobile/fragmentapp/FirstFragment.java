d24097295d4438414b62700d2af78cb082c8fd1b
murager <murager@codebusters.team>
2019-09-27 09:39:22 +0600

    Button click init and click added.

--- app/src/main/java/kz/mobile/fragmentapp/FirstFragment.java
+++ app/src/main/java/kz/mobile/fragmentapp/FirstFragment.java
@@ -3,14 +3,19 @@ package kz.mobile.fragmentapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstFragment extends Fragment {
 
+    private Button button;
 
     public FirstFragment() {
         // Required empty public constructor
@@ -23,5 +28,16 @@ public class FirstFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
-
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        button = view.findViewById(R.id.button); //Init button in Fragment with view
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Show toast on click
+                Toast.makeText(getContext(), "Hello", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
