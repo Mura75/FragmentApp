129c4381e8e8b300902e14d31c9556b961c6bdc6
murager <murager@codebusters.team>
2019-09-27 09:35:08 +0600

    Init commit.

---
+++ app/src/main/java/kz/mobile/fragmentapp/MainActivity.java
@@ -0,0 +1,20 @@
+package kz.mobile.fragmentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Show fragment in container
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, new FirstFragment())
+                .commit();
+    }
+}
